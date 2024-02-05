02f7df922169dde226ac88f210a69e135706285f
N-lara <n_lara01@yahoo.com>
2024-02-05 16:45:43 -0600

    added react router dom implimentation

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <main>
+      <div className="">
+        <div className="">
+          <h1>homepage</h1>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
