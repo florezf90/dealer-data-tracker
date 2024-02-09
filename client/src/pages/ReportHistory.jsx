32957c565fae78fcd746d5a368768b3bfed2395d
Jbradu2 <Joshua.ry.bradshaw@gmail.com>
2024-02-09 16:21:34 -0600

    added tags for report history

--- client/src/pages/ReportHistory.jsx
+++ client/src/pages/ReportHistory.jsx
@@ -1,63 +1,68 @@
-import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { GET_DEALER,  } from '../utils/queries'; // Import the GraphQL query
-import AuthService from '../utils/auth';
-import Auth from '../utils/auth';
-import { useParams } from 'react-router-dom';
-
-
-
+import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_DEALER } from "../utils/queries"; // Import the GraphQL query
+import AuthService from "../utils/auth";
+import Auth from "../utils/auth";
+import { useParams } from "react-router-dom";
 
 const ReportHistory = () => {
-  const {dealerId}= useParams();
+  const { dealerId } = useParams();
   const { loading, error, data } = useQuery(GET_DEALER, {
-    variables: { 
+    variables: {
       email: AuthService.getUserIdFromToken(),
-      _id:dealerId
-    }
+      _id: dealerId,
+    },
   });
-  console.log('query data: ' +data);
+  console.log("query data: " + data);
 
-console.log(data)
-    const logout = (event) => {
+  console.log(data);
+  const logout = (event) => {
     event.preventDefault();
     Auth.logout();
-  }
+  };
 
   if (!AuthService.loggedIn()) {
-    window.location.assign('/login');    
+    window.location.assign("/login");
   }
 
-// Check if data is undefined or dealers // dealers array is empty
-if (!data || !data.dealer || !data.dealer.reports || data.dealer.reports.length === 0) {
+  // Check if data is undefined or dealers // dealers array is empty
+  if (
+    !data ||
+    !data.dealer ||
+    !data.dealer.reports ||
+    data.dealer.reports.length === 0
+  ) {
+    return (
+      <div className="report-history">
+        <h2>Report History</h2>
+        <p>No reports available</p>
+        <Link to={"/add-report/" + dealerId}>
+          <button>Add Report</button>
+        </Link>
+      </div>
+    );
+  }
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :</p>;
+
   return (
     <div className="report-history">
-      <h2>Report History</h2>
-      <p>No reports available</p>
-      <Link to={"/add-report/"+dealerId}>
-        <button>Add Report</button>
-      </Link>
-    </div>
-  );
-}
-
-if (loading) return <p>Loading...</p>;
-if (error) return <p>Error :</p>;
-
-return (
-<div className="report-history">
       <h2>Report History</h2>
       <div className="history">
         <ul>
           {data.dealer.reports.map((report, index) => (
             <li key={index}>
-              {report._id}{data.dealer.lastName +  ", " +data.dealer.firstName + ": "}
-              {report.handsDealt + " "}
-              {report.promotionTaken + " "}
-              {report.moneyTaken + " "}
-              {report.createdAt}
+              <p>
+                Dealer :{report._id}
+                {data.dealer.lastName + ", " + data.dealer.firstName + ": "}
+              </p>
+              <p>Hands Dealt: {report.handsDealt + " "}</p>
+              <p>Promotion Taken: {report.promotionTaken + " "}</p>
+              <p>Money Taken: {report.moneyTaken + " "}</p>
+              <p>Report Date: {report.createdAt}</p>
             </li>
-          ))} 
+          ))}
         </ul>
       </div>
       {Auth.loggedIn() && (
@@ -66,6 +71,6 @@ return (
         </button>
       )}
     </div>
-);
+  );
 };
 export default ReportHistory;
