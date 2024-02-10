6cd98c26e743e0c253be1477082f28249df12244
unknown <florezf90@gmail.com>
2024-02-10 16:02:35 -0600

    created navbar component for the home page and also added functionality to the buttons on display, also I make sure that the navbar has proper responsive desing depending of the screen sizes

---
+++ client/src/components/dropdown/dropdown.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { NavDropdown } from 'react-bootstrap';
+import Auth from '../../utils/auth';
+
+
+function CustomDropdown({ title, options }) {
+  const handleLogout = () => {
+    Auth.logout();
+  }
+  return (
+    <NavDropdown title={title} id="basic-nav-dropdown">
+      {options.map((option, index) => (
+        <NavDropdown.Item key={index} href={option.href}>{option.label}</NavDropdown.Item>
+      ))}
+      {!Auth.loggedIn() && (
+        <>
+          <NavDropdown.Item href="/login">Log-in</NavDropdown.Item>
+          <NavDropdown.Item href="/Signup">Sign-up</NavDropdown.Item>
+        </>
+      )}
+      {Auth.loggedIn() && (
+        <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
+      )}
+    </NavDropdown>
+  );
+}
+
+export default CustomDropdown;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,65 +1,39 @@
-// import { Link } from 'react-router-dom';
-// import Auth from '../utils/auth';
-// import Container from 'react-bootstrap/Container';
-// import Col from 'react-bootstrap/Col';
-//  import Row from 'react-bootstrap/Row';
+import { Link } from 'react-router-dom';
+import Auth from '../utils/auth';
+import MainNavbar from '../components/navbar/navbar';
 
 
 
 
-// const Home = () => {
+const Home = () => {
 
-  // const logout = (event) => {
-  //   event.preventDefault();
-  //   Auth.logout();
-  // }
-//   return (
-// <Container>
-//       <Row>
-//         <Col>1 of 1</Col>
-//       </Row>
-//     </Container>
-    // <main>
-    //   <div className="">
-    //     <div className="">
-    //       <h1>welcome to the homepage</h1>
-    //       <Link className="btn btn-lg btn-primary m-2" to="/login">
-    //         login
-    //       </Link>
-    //       <br/>
-    //       <Link className="btn btn-lg btn-primary m-2" to="/signup">
-    //         sign-up
-    //       </Link>
-    //       {Auth.loggedIn() && (
-    //         <button className="btn btn-lg btn-light m-2" onClick={logout}>
-    //           Logout
-    //         </button>
-    //       )}
-    //     </div>
-    //   </div>
-    // </main>
-//   );
-// };
-
-// export default Home;
-
-
-import Dropdown from 'react-bootstrap/Dropdown';
-
-function BasicExample() {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  }
   return (
-    <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+    <main>
+      <MainNavbar />
+      <div className="">
+        <div className="">
+          <h1>welcome to the homepage</h1>
+          <Link className="btn btn-lg btn-primary m-2" to="/login">
+            login
+          </Link>
+          <br/>
+          <Link className="btn btn-lg btn-primary m-2" to="/signup">
+            sign-up
+          </Link>
+          {Auth.loggedIn() && (
+            <button className="btn btn-lg btn-light m-2" onClick={logout}>
+              Logout
+            </button>
+          )}
+        </div>
+      </div>
+    </main>
   );
-}
+};
 
-export default BasicExample;
+export default Home;
+
